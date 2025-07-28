75918211c13753a75ec3b0c93533bb5c9f88ec17
Minju Kim <minjugim655@gmail.com>
2025-07-28 15:32:13 +0900

    Add generator functions for number generation

---
+++ generator_01.py
@@ -0,0 +1,18 @@
+# 전부 생산 후 출하
+def generate_numbers_1(max_number: int):
+    print('Called generate_numbers_1 function')
+    numbers = []
+    for i in range(1, max_number+1):
+        numbers.append(i)
+    return numbers
+
+# Generator 객체를 생성하는 함수
+def generate_numbers_2(max_number: int):
+    print('Called generate_numbers_2 function')
+    for i in range(1, max_number+1):
+        yield i 
+
+numbers_1 = generate_numbers_1(10)
+numbers_2 = generate_numbers_2(10)
+print(numbers_1)
+print(numbers_2)
